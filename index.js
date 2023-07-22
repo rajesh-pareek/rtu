e843ce76e3e1d9acddb7dc15351994e6e7bdb3b9
rajesh-pareek <rajeshpareekdevo@gmail.com>
2023-07-22 23:21:22 +0530

    second-commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const textToType = "My journey as a warrior is one of purpose and honor. I take pride in upholding my principles and embracing my responsibilities with integrity. While the battles I face may be challenging, I am resolute in my pursuit of righteousness and the betterment of the world around me."; // The text you want to auto-type
+const typingSpeed = 20; // Adjust this value to control the typing speed (in milliseconds)
+const typingTextElement = document.getElementById("typing-text");
+
+let charIndex = 0;
+
+
+
+function typeText() {
+    if (charIndex < textToType.length) {
+        typingTextElement.textContent += textToType.charAt(charIndex);
+        charIndex++;
+        setTimeout(typeText, typingSpeed);
+    }
+}
+
+typeText();
+
+function togglePopUp() {
+    const popUpBox = document.getElementById("popUpBox");
+    popUpBox.classList.toggle("pop-up-visible");
+}
+const fun=togglePopUp;
+
+setTimeout(fun,7000);
+
+
+
+// Adjust the delay (in milliseconds) to control when the animation starts
